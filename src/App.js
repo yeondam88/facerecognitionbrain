42235b04304c8d80ea3df2bb661437d4d13e98df
Yeondam Park <giyen9088@gmail.com>
2018-02-07 23:36:45 -0800

    Adding functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import { API_KEY } from "./API";
 import "./App.css";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+const app = new Clarifai.App({
+  apiKey: API_KEY
+});
+
 class App extends Component {
+  state = {
+    input: ""
+  };
+
+  onInputChange = event => {};
+
+  onButtonSubmit = () => {
+    console.log("Click!");
+  };
+
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
-        {/*        <Logo />
-        <ImageLinkForm />
-<FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
       </div>
     );
   }
