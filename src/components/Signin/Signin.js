6898aa6eb13bfe01dfe23c53f029baf58aaa614b
Yeondam Park <giyen9088@gmail.com>
2018-02-19 22:29:12 -0800

    found the bug..**form tag changed to div tag**

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,30 +19,28 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch(`http://localhost:3000/signin/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: this.state.signInEmail,
         password: this.state.signInPassword
       })
-        .then(response => response.json())
-        .then(user => {
-          if (user.id) {
-            this.props.loadUser(user);
-            this.props.onRouteChange("home");
-          }
-        })
-    });
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
+        }
+      });
   };
 
   render() {
     return (
       <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <form className="measure">
+          <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
@@ -86,7 +84,7 @@ class Signin extends React.Component {
                 Register
               </p>
             </div>
-          </form>
+          </div>
         </main>
       </article>
     );
